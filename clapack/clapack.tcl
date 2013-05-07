7d82854a2f19d5a385135ac87c6ce540774cdd9b
Chris Scheller <schelcj@umich.edu>
2013-05-07 11:52:30 -0400

    new module

---
+++ clapack/clapack.tcl
@@ -0,0 +1,3 @@
+module-whatis "Description: The CLAPACK library was built using a Fortran to C conversion utility called f2c. The entire Fortran 77 LAPACK library is run through f2c to obtain C code, and then modified to improve readability. CLAPACK's goal is to provide LAPACK for someone who does not have access to a Fortran compiler."
+module-whatis "Vendor: http://www.netlib.org/clapack/"
+module-whatis "Licenses: N/A"
